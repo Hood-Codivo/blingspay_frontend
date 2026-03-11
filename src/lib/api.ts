fc7920cf75ae857dc5eceae958d20679ace35db2
Bakpo Godwin <67370368+Hood-Codivo@users.noreply.github.com>
2026-03-11 17:49:03 +0100

    env added into the code

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL = import.meta.env.PROD
   ? import.meta.env.VITE_API_BASE_URL_PROD
-  : "http://localhost:8000/api";
+  : "https://blingspay-backend.onrender.com/api";
 
 const api = axios.create({
   baseURL,
